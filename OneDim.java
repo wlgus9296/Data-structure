1b958b2c77438797e01e034c60d910c4d3b093d8
wlgus9296 <79959593+wlgus9296@users.noreply.github.com>
2021-03-10 16:16:45 +0900

    Add files via upload

---
+++ OneDim.java
@@ -0,0 +1,19 @@
+
+public class OneDim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sale[] = new int[4];
+		sale[0] = 157;
+		sale[1] = 209;
+		sale[2] = 251;
+		sale[3] = 312;
+		
+		
+		for(int i=0; i<4; i++)
+			System.out.println(sale[i]);
+		
+
+	}
+
+}
---
+++ ThreeDim.java
@@ -0,0 +1,24 @@
+
+public class ThreeDim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sale[][][] = new int [][][] {{{63,84,140,130},
+										{157,209,254,312}},
+										{{59,80,130,135},
+										{149,187,239,310}}};
+										
+		for(int i=0; i<2; i++) {
+			System.out.printf("<<%d ÆÀ >> %n", i+1);
+			for(int j=0; j<2; j++) {
+				for(int k=0; k<4; k++)
+					System.out.printf("%d/4ºÐ±â : sale[%d][%d][%d] = %d %n", k+1,i,j,k,sale[i][j][k]);
+				System.out.println("--------------------");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
